bba146a60514c86926fbcb8adb19081237d4baf4
Teos WorkStation <teos@otaku.gmail.com>
2022-03-24 17:01:06 +0300

    fix task01

--- Task01/script.sh
+++ Task01/script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-dnf install -y httpd
-dnf install -y mysql-server
-dnf install -y php
+
+sudo yum update -y
+sudo yum install -y httpd
+sudo yum install -y mysql-server
+sudo yum install -y php
+sudo yum install -y mc
 
 
-systemctl enable httpd
-systemctl start httpd
-systemctl start mysqld
+sudo systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl start mysqld
