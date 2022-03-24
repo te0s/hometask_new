10eb94c0ad74816bed892b91e0a679b510c0c20b
teos home <teos.otaku@gmail.com>
2022-03-24 22:43:21 +0300

    make centos vm for site v0.1

--- Task01/script.sh
+++ Task01/script.sh
@@ -11,3 +11,36 @@ sudo yum install -y mc
 sudo systemctl enable httpd
 sudo systemctl start httpd
 sudo systemctl start mysqld
+
+sudo rm /etc/httpd/sites-available/01-demosite-static.conf
+sudo touch /etc/httpd/sites-available/01-demosite-static.conf
+sudo cat <<PAST | tee -a /etc/httpd/sites-available/01-demosite-static.conf
+<VirtualHost *:80>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html/01-demosite-static
+        DirectoryIndex index.html
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+PAST
+
+sudo rm /etc/httpd/sites-available/01-demosite-php.conf
+sudo touch /etc/httpd/sites-available/01-demosite-php.conf
+sudo cat <<PAST | tee -a /etc/httpd/sites-available/01-demosite-php.conf
+<VirtualHost *:81>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html/01-demosite-php
+        DirectoryIndex index.php
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+PAST
+
+sudo sudo a2ensite 01-demosite-static.conf
+sudo a2ensite 01-demosite-php.conf
+sudo rm /etc/httpd/sites-enabled/000-default.conf
+
+sudo cp /etc/httpd/ports.conf.bak /etc/httpd/ports.conf
+sudo sed -i.bak -e "/Listen 80/a Listen 81" /etc/httpd/ports.conf
+
+sudo service httpd restart
