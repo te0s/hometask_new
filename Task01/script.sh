bd2474ce252a8ad3233cbf8e1573580dcc37265c
teos home <teos.otaku@gmail.com>
2022-03-24 23:11:08 +0300

    fit for CenOS8

--- Task01/script.sh
+++ Task01/script.sh
@@ -12,35 +12,31 @@ sudo systemctl enable httpd
 sudo systemctl start httpd
 sudo systemctl start mysqld
 
-sudo rm /etc/httpd/sites-available/01-demosite-static.conf
-sudo touch /etc/httpd/sites-available/01-demosite-static.conf
-sudo cat <<PAST | tee -a /etc/httpd/sites-available/01-demosite-static.conf
+#added sites conf
+sudo touch /etc/httpd/conf.d/01-demosite-static.conf
+cat <<PAST | sudo tee /etc/httpd/conf.d/01-demosite-static.conf
 <VirtualHost *:80>
         ServerAdmin webmaster@localhost
         DocumentRoot /var/www/html/01-demosite-static
         DirectoryIndex index.html
-        ErrorLog \${APACHE_LOG_DIR}/error.log
-        CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 PAST
 
-sudo rm /etc/httpd/sites-available/01-demosite-php.conf
-sudo touch /etc/httpd/sites-available/01-demosite-php.conf
-sudo cat <<PAST | tee -a /etc/httpd/sites-available/01-demosite-php.conf
+sudo touch /etc/httpd/conf.d/01-demosite-php.conf
+cat <<PAST | sudo tee /etc/httpd/conf.d/01-demosite-php.conf
 <VirtualHost *:81>
         ServerAdmin webmaster@localhost
         DocumentRoot /var/www/html/01-demosite-php
         DirectoryIndex index.php
-        ErrorLog \${APACHE_LOG_DIR}/error.log
-        CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 PAST
 
-sudo sudo a2ensite 01-demosite-static.conf
-sudo a2ensite 01-demosite-php.conf
-sudo rm /etc/httpd/sites-enabled/000-default.conf
+sudo sed -i.bak -e "/Listen 80/a Listen 81" /etc/httpd/conf/httpd.conf
 
-sudo cp /etc/httpd/ports.conf.bak /etc/httpd/ports.conf
-sudo sed -i.bak -e "/Listen 80/a Listen 81" /etc/httpd/ports.conf
+sudo rm /etc/httpd/conf.d/welcome.conf
+sudo chmod -R 775 /var/www/html
+sudo restorecon -R /var/www/html/
 
-sudo service httpd restart
+#restart
+sudo systemctl restart httpd
+
