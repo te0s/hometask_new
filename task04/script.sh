755988d88a2f499bd65a39da98b172b834e78789
Teos WorkStation <teos@otaku.gmail.com>
2022-04-12 20:56:04 +0300

    all vm online

--- task04/script.sh
+++ task04/script.sh
@@ -4,3 +4,4 @@ vagrant destroy master.puppet -f
 vagrant destroy slave2.puppet -f
 vagrant destroy slave1.puppet -f
 #ansible -i /vagrant/inventory/hosts all -m ping
+#ansible-playbook -i /vagrant/inventory/hosts /vagrant/playbook.yaml
